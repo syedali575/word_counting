1f77fc05a7fa194627e39e89a0f34a9b49b284e5
Syed Ali <sali3@cox.net>
2016-10-25 13:39:49 -0400

    Was able to split paragraph of text and get it in a array

---
+++ js/hw.js
@@ -0,0 +1,15 @@
+
+
+var hwString = "From fairest creatures we desire increase, That thereby beauty's rose might never die, But as the riper should by time decease, His tender heir might bear his memory: But thou contracted to thine own bright eyes, Feed'st thy light's flame with self-substantial fuel, Making a famine where abundance lies, Thy self thy foe, to thy sweet self too cruel: Thou that art now the world's fresh ornament, And only herald to the gaudy spring, Within thine own bud buriest thy content, And tender churl mak'st waste in niggarding: Pity the world, or else this glutton be, To eat the world's due, by the grave and thee";
+var space = " ";
+
+
+function split(stringToSplit, separator) {
+  var arrayOfStrings = stringToSplit.split(separator);
+  return arrayOfStrings;
+}
+
+
+split(hwString, space);
+
+console.log(split(hwString, space));
